c27c8fd64236c802e977b311ec9d634aaef81673
juditcanovas <juditcanovas9@gmail.com>
2025-06-18 19:47:36 +0200

    Agregado contador intentos

--- adivina_numero.py
+++ adivina_numero.py
@@ -1,28 +1,45 @@
 import random
-numero_secreto = random.randint(1, 20) #randint() es una función específica dentro de random que genera un número entero aleatorio (de ahí "rand-int").
 
-print('Estoy pensando en un número del 1 al 20...')
-print('¿Puedes adivinarlo?')
+def adivina_numero():
+    numero_secreto = random.randint(1, 20) #randint() es una función específica dentro de random que genera un número entero aleatorio (de ahí "rand-int").
+    intentos_maximos = 5
+    intentos_realizados = 0
 
-suposicion_int = 0 #Se pone para que se inicie el bulge
+    print('Estoy pensando en un número del 1 al 20...')
+    print('¿Puedes adivinarlo?')
+    print(f'Tines {intentos_maximos} intentos. ¿Te ves capaz?')
 
-while suposicion_int != numero_secreto:
-    try:
-        suposicion_str=input('Adivina el número:')
-        suposicion_int = int(suposicion_str)
-    except ValueError:
-        print('Mmm... Creo que eso no es un número.')
-        continue
+    suposicion_int = 0 #Se pone para que se inicie el bulge
 
+    while suposicion_int != numero_secreto and intentos_realizados < intentos_maximos:
+        try:
+            suposicion_str=input('Adivina el número:')
+            suposicion_int = int(suposicion_str)
+        except ValueError:
+            print('Mmm... Creo que eso no es un número.')
+            continue
 
+        intentos_realizados += 1
 
-    if suposicion_int == numero_secreto:
-         print('Felicidades has acertado, ese era mi número.')
+        if suposicion_int == numero_secreto:
+            print(f'Felicidades has acertado, ese era mi número. Has acertado con {intentos_realizados} intentos.')
+            break # Salir del bucle
+        elif suposicion_int < numero_secreto:
+            print('Estás cerca, mi número es mayor.')
+        else:
+            print('Casi, te pasaste, el número menor.')
+        
+        if suposicion_int != numero_secreto and intentos_realizados<intentos_maximos:
+            intentos_restantes = intentos_maximos - intentos_realizados
+            print(f'Te quedan {intentos_restantes} intentos.')
     
-    elif suposicion_int < numero_secreto:
-        print('Estás cerca, mi número es mayor.')
+    if suposicion_int != numero_secreto and intentos_realizados == intentos_maximos:
+        print(f'¡Oh no! Te has quedado sin intentos lo siento. El número secreto era {numero_secreto}.')
 
-    else:
-        print('Casi, te pasaste, el número menor.')
+    print('¡Gracias por jugar! :D')
 
-print('¡Gracias por jugar! :D')
+adivina_numero()
+
+jugar_otra_vez = True
+
+
